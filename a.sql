e1a9817848e58dbd84ac09e318cf20599d39812b
Wang Jie <doudou19758@gmail.com>
2016-11-30 15:38:57 +0800

    add sql file

---
+++ a.sql
@@ -0,0 +1,39 @@
+-- Adminer 4.2.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game` varchar(255) COLLATE utf8_bin NOT NULL,
+  `owner` varchar(255) COLLATE utf8_bin NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '0',
+  `limit` int(11) NOT NULL DEFAULT '0',
+  `permissions` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+DROP TABLE IF EXISTS `keys`;
+CREATE TABLE `keys` (
+  `key` varchar(255) COLLATE utf8_bin NOT NULL,
+  `used` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `value` int(11) NOT NULL,
+  KEY `used` (`used`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `token` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+-- 2016-11-30 07:37:33
